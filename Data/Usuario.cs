70036946c1fb9b40ba362160c3abbc1c39405af4
NiiHaL <71907716+Nihal182@users.noreply.github.com>
2022-12-10 12:45:53 -0300

    Testando insercao do méetodo POST com ADICIONAR USUARIO

---
+++ Data/Usuario.cs
@@ -0,0 +1,29 @@
+using Newtonsoft.Json;
+
+namespace Centralmanutencao.Data;
+
+public class Usuario
+{
+    
+    [JsonProperty("Nome")]
+    public string? Nome { get; set; }
+
+    private string? senha1;
+
+    public string? GetSenha1()
+    {
+        return senha1;
+    }
+
+    public void SetSenha1(string? value)
+    {
+        senha1 = value;
+    }
+
+    [JsonProperty("DataCriacao")]
+    public DateTime DataCriacao { get ; set; }
+
+    [JsonProperty("DataAtual")]
+     public DateTime DataAtual { get; set; } = DateTime.Now;
+
+}
